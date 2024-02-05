a72f6d94559e821d20e10a55a77e4eaeef775f92
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-02-05 20:05:16 +0800

    feat: users login and register v1

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'nis',
+        'address',
+        'phone_number',
         'email',
         'password',
     ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('nis');
+            $table->integer('nis')->nullable();
             $table->string('name');
-            $table->string('address');
-            $table->string('phone_number');
+            $table->string('address')->nullable();
+            $table->integer('phone_number')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
